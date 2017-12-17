52be2c29584c41fd84013dde8b3408f44b68662b
Aux <justin.gentry@auxesis.tv>
2017-12-17 16:35:06 -0600

    More controller

---
+++ src/Controllers/Collie/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Colliebot.Api.Rest.Controllers.Collie
+{
+    [Route("user")]
+    public class UserController : Controller
+    {
+        [HttpGet("{id}", Name = nameof(GetUserAsync))]
+        public async Task<IActionResult> GetUserAsync(ulong id)
+        {
+            await Task.Delay(0);
+            return Ok();
+        }
+    }
+}
---
+++ src/Controllers/Collie/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Colliebot.Api.Rest.Controllers.Collie
+{
+    [Route("users")]
+    public class UsersController : Controller
+    {
+        [HttpGet(Name = nameof(GetUsersAsync))]
+        public async Task<IActionResult> GetUsersAsync(string discrim = null, DateTime? createdAt = null)
+        {
+            await Task.Delay(0);
+            return Ok();
+        }
+
+        [HttpGet("count", Name = nameof(GetUsersCountAsync))]
+        public async Task<IActionResult> GetUsersCountAsync(DateTime? createdAt = null)
+        {
+            await Task.Delay(0);
+            return Ok();
+        }
+    }
+}
